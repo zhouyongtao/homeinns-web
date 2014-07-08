4ae118437616e6d0c962f40e67ae0280023702cd
Irving <zhouyongtao@outlook.com>
2014-07-08 23:57:23 +0800

    添加代码

--- src/main/java/com/homeinns/web/controller/HomeController.java
+++ src/main/java/com/homeinns/web/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.homeinns.web.controller;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Irving on 2014/7/6.
@@ -26,15 +29,24 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/get" ,method = RequestMethod.GET)
-    public String hello(HttpSession session,HttpServletRequest request,HttpServletResponse response) {
-        Object obj= session.getAttribute("key");
-        if(obj!=null) {
-            try {
-                 response.getWriter().print(obj.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
+    public void hello(HttpSession session,HttpServletResponse response) {
+        PrintWriter out = null;
+        try
+        {
+            response.setContentType("text/json;charset=utf-8");
+            out = response.getWriter();
+            Object obj= session.getAttribute("key");
+            if(obj!=null) {
+                out.write(obj.toString());
             }
         }
-        return "/get";
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (out != null){ out.close();}
+        }
     }
 }
