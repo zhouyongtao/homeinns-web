97ce5de6b58a8102ebf792e777853d97648c9e1b
irving <zhoyongtao@outlook.com>
2014-07-08 10:21:30 +0800

    change test code!

--- src/main/java/com/homeinns/web/controller/AjaxController.java
+++ src/main/java/com/homeinns/web/controller/AjaxController.java
@@ -2,7 +2,7 @@ package com.homeinns.web.controller;
 import org.springframework.stereotype.Controller;
 
 /**
- * Created by Administrator on 2014/7/7.
+ * Created by Irving on 2014/7/7.
  */
 @Controller
 public class AjaxController {
--- src/test/java/com/homeinns/web/controller/HomeControllerTest.java
+++ src/test/java/com/homeinns/web/controller/HomeControllerTest.java
@@ -1,8 +1,11 @@
 package com.homeinns.web.controller;
 import junit.framework.TestCase;
-
 public class HomeControllerTest extends TestCase {
 
+    /*
+    *
+    *   shift + ctrl + T  生成测试类
+    * */
     public void testIndex() throws Exception {
 
     }
