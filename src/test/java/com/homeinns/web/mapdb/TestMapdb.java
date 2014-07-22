6b6264430681c294fce5b81d0305d3b839855c9c
irving <zhoyongtao@outlook.com>
2014-07-22 14:16:01 +0800

    add test mandb code

--- src/test/java/com/homeinns/web/mapdb/TestMapdb.java
+++ src/test/java/com/homeinns/web/mapdb/TestMapdb.java
@@ -11,23 +11,23 @@ public class TestMapdb {
 
    @Test
    public void test(){
-       // configure and open database using builder pattern.
-       // all options are available with code auto-completion.
+       //configure and open database using builder pattern.
+       //all options are available with code auto-completion.
        DB db = DBMaker.newFileDB(new File("testdb"))
                .closeOnJvmShutdown()
                .encryptionEnable("password")
                .make();
-       // open existing an collection (or create new)
+       //open existing an collection (or create new)
        ConcurrentNavigableMap map = db.getTreeMap("collectionName");
        map.put(1, "one");
        map.put(2, "two");
-       // map.keySet() is now [1,2]
+       //map.keySet() is now [1,2]
        //persist changes into disk
        db.commit();
        map.put(3, "three");
-       // map.keySet() is now [1,2,3]
+       //map.keySet() is now [1,2,3]
        db.rollback(); //revert recent changes
-       // map.keySet() is now [1,2]
+       //map.keySet() is now [1,2]
        db.close();
    }
 }
