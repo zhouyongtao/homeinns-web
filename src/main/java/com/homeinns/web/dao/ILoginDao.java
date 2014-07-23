bb946f11f3a0b62f2f8bf7a80d0337ae18d13538
irving <zhoyongtao@outlook.com>
2014-07-23 13:38:57 +0800

    change login interface code

---
+++ src/main/java/com/homeinns/web/dao/ILoginDao.java
@@ -0,0 +1,8 @@
+package com.homeinns.web.dao;
+
+/**
+ * Created by ytzhou on 2014/7/23.
+ */
+public interface ILoginDao {
+
+}
---
+++ src/main/java/com/homeinns/web/dao/impl/LoginDao.java
@@ -0,0 +1,11 @@
+package com.homeinns.web.dao.impl;
+import com.homeinns.web.dao.ILoginDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by ytzhou on 2014/7/23.
+ */
+@Repository
+public class LoginDao implements ILoginDao {
+
+}
