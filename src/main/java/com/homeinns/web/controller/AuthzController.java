bc784a8f4451aaf294345785e718ba364829759e
Irving <zhouyongtao@outlook.com>
2014-11-23 00:31:03 +0800

    add oautu2 authorization

--- src/main/java/com/homeinns/web/controller/AuthzController.java
+++ src/main/java/com/homeinns/web/controller/AuthzController.java
@@ -1,7 +1,10 @@
 package com.homeinns.web.controller;
+
+import org.apache.oltu.oauth2.as.issuer.MD5Generator;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuer;
 import org.apache.oltu.oauth2.as.issuer.OAuthIssuerImpl;
-import org.apache.oltu.oauth2.as.issuer.UUIDValueGenerator;
 import org.apache.oltu.oauth2.as.request.OAuthAuthzRequest;
+import org.apache.oltu.oauth2.as.request.OAuthTokenRequest;
 import org.apache.oltu.oauth2.as.response.OAuthASResponse;
 import org.apache.oltu.oauth2.common.OAuth;
 import org.apache.oltu.oauth2.common.error.OAuthError;
@@ -10,14 +13,13 @@ import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
@@ -31,19 +33,22 @@ public class AuthzController {
 
     private static Logger logger = LoggerFactory.getLogger(AuthzController.class);
 
-    /**
+     /* *
      * 构建OAuth2授权请求 [需要client_id与redirect_uri绝对地址]
      * @param request
      * @param response
+     * @return
      * @throws OAuthSystemException
      * @throws IOException
      * @url  http://localhost:8080/oauth2/authorize?client_id={AppKey}&response_type=code&redirect_uri={YourSiteUrl}
-     * @test http://localhost:8080/oauth2/authorize?client_id=fbed1d1b4b1449daa4bc49397cbe2350&response_type=code&redirect_uri=http://baidu.com
+     * @test http://localhost:8080/oauth2/authorize?client_id=fbed1d1b4b1449daa4bc49397cbe2350&response_type=code&redirect_uri=http://baidu.comx
      */
     @RequestMapping(value = "/authorize",method = RequestMethod.GET)
-    public Object authorize(HttpServletRequest request, HttpServletResponse response)
+    public void authorize(HttpServletRequest request, HttpServletResponse response)
             throws OAuthSystemException, IOException {
+        PrintWriter out = null;
         try {
+            out = response.getWriter();
             //获得请求的参数信息
             OAuthAuthzRequest oauthRequest = new OAuthAuthzRequest(request);
             //验证appkey是否正确
@@ -51,30 +56,92 @@ public class AuthzController {
                 OAuthResponse oauthResponse = OAuthASResponse
                                               .errorResponse(HttpServletResponse.SC_OK)
                                               .setError(OAuthError.TokenResponse.INVALID_CLIENT)
-                                              .setErrorDescription("VERIFY_CLIENTID_ERROR")
+                                              .setErrorDescription("VERIFY_CLIENTID_FAIL")
                                               .buildJSONMessage();
-                return new ResponseEntity(oauthResponse.getBody(), HttpStatus.valueOf(oauthResponse.getResponseStatus()));
+                out.write(oauthResponse.getBody());
+                out.flush();
+                out.close();
+                return;
             }
           //构建oauth2授权请求
           OAuthResponse oauthResponse = OAuthASResponse
                                .authorizationResponse(request, HttpServletResponse.SC_FOUND)
                                 //UUIDValueGenerator OR MD5Generator
-                               .setCode(new OAuthIssuerImpl(new UUIDValueGenerator()).authorizationCode())
+                               .setCode(new OAuthIssuerImpl(new MD5Generator()).authorizationCode())
                                .location(oauthRequest.getParam(OAuth.OAUTH_REDIRECT_URI))
                                .buildQueryMessage();
-           return "redirect:"+oauthResponse.getLocationUri();
+            response.sendRedirect(oauthResponse.getLocationUri());
         } catch(OAuthProblemException ex) {
             String redirectUri = ex.getRedirectUri();
+            //302地址为空
             if (redirectUri==null||redirectUri.length()==0) {
-                return new ResponseEntity(
-                        "OAuth callback url needs to be provided !", HttpStatus.NOT_FOUND);
+                out.write("oauth2 callback url needs to be provided");
+                out.flush();
+                out.close();
+                return;
             }
+            //处理异常
             final OAuthResponse oauthResponse = OAuthASResponse
                                                 .errorResponse(HttpServletResponse.SC_FOUND)
                                                 .error(ex)
                                                 .location(redirectUri)
                                                 .buildQueryMessage();
-             return "redirect:"+oauthResponse.getLocationUri();
+            response.sendRedirect(oauthResponse.getLocationUri());
+        }
+        finally
+        {
+            if (null != out){ out.close();}
+        }
+    }
+
+
+    /**
+     * 获取令牌(AccessToken)
+     * @param request
+     * @param response
+     * @return
+     * @url http://localhost:8080/oauth2/access_token?client_id={AppKey}&client_secret={AppSecret}&grant_type=authorization_code&redirect_uri={YourSiteUrl}&code={code}
+     */
+    @RequestMapping(value = "/access_token",method = RequestMethod.GET)
+    public void access_token(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, OAuthSystemException {
+        PrintWriter out = null;
+        OAuthTokenRequest oauthRequest = null;
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+        try {
+            out = response.getWriter();
+            oauthRequest = new OAuthTokenRequest(request);
+            //validateClient(oauthRequest);
+            String authzCode = oauthRequest.getCode();
+            // some code
+            String accessToken = oauthIssuerImpl.accessToken();
+            String refreshToken = oauthIssuerImpl.refreshToken();
+            // some code
+
+            OAuthResponse r = OAuthASResponse
+                    .tokenResponse(HttpServletResponse.SC_OK)
+                    .setAccessToken(accessToken)
+                    .setExpiresIn("3600")
+                    .setRefreshToken(refreshToken)
+                    .buildJSONMessage();
+            response.setStatus(r.getResponseStatus());
+            out.print(r.getBody());
+            out.flush();
+            out.close();
+        } catch(OAuthProblemException ex) {
+            OAuthResponse r = OAuthResponse
+                    .errorResponse(401)
+                    .error(ex)
+                    .buildJSONMessage();
+            response.setStatus(r.getResponseStatus());
+            out.print(r.getBody());
+            out.flush();
+            out.close();
+            response.sendError(401);
+        }
+        finally
+        {
+            if (null != out){ out.close();}
         }
     }
 
@@ -85,10 +152,10 @@ public class AuthzController {
      */
     public boolean validateRedirectionURI(OAuthAuthzRequest oauthRequest) {
         //客户端Appkey
-        ArrayList clientKey = new  ArrayList();
-        clientKey.add("fbed1d1b4b1449daa4bc49397cbe2350");
-        clientKey.add("a85b033590714fafb20db1d11aed5497");
-        clientKey.add("d23e06a97e2d4887b504d2c6fdf42c0b");
-        return clientKey.contains(oauthRequest.getClientId());
+        ArrayList arrayKeys = new  ArrayList();
+        arrayKeys.add("fbed1d1b4b1449daa4bc49397cbe2350");
+        arrayKeys.add("a85b033590714fafb20db1d11aed5497");
+        arrayKeys.add("d23e06a97e2d4887b504d2c6fdf42c0b");
+        return arrayKeys.contains(oauthRequest.getClientId());
     }
 }
