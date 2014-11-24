74a55e7edf2145607b9083654b263c966f202b06
Irving <zhouyongtao@outlook.com>
2014-11-24 20:01:12 +0800

    update oauth service

--- src/main/java/com/homeinns/web/controller/ResourceController.java
+++ src/main/java/com/homeinns/web/controller/ResourceController.java
@@ -1,7 +1,13 @@
 package com.homeinns.web.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Irving on 2014/11/23.
  */
+@Controller
+@RequestMapping("/oauth2")
 public class ResourceController {
+
+
 }
