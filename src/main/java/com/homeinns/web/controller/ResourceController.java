87c94cd6cb7e9c15bb4a9c6b1131fadee1c14fb6
irving <zhouyongtao@outlook.com>
2014-11-26 17:34:07 +0800

    update resource controller

--- src/main/java/com/homeinns/web/controller/ResourceController.java
+++ src/main/java/com/homeinns/web/controller/ResourceController.java
@@ -1,12 +1,10 @@
 package com.homeinns.web.controller;
-import org.apache.oltu.oauth2.as.response.OAuthASResponse;
 import org.apache.oltu.oauth2.common.OAuth;
 import org.apache.oltu.oauth2.common.error.OAuthError;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
 import org.apache.oltu.oauth2.common.message.types.ParameterStyle;
-import org.apache.oltu.oauth2.common.utils.OAuthUtils;
 import org.apache.oltu.oauth2.rs.request.OAuthAccessResourceRequest;
 import org.apache.oltu.oauth2.rs.response.OAuthRSResponse;
 import org.slf4j.Logger;
