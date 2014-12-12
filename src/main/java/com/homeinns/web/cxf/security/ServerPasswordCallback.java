5a784ab5754f23337a135c4a04c4fc78d400ec70
irving <zhouyongtao@outlook.com>
2014-12-12 18:52:08 +0800

    update cxf

---
+++ src/main/java/com/homeinns/web/cxf/security/ServerPasswordCallback.java
@@ -0,0 +1,30 @@
+package com.homeinns.web.cxf.security;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+/**
+ * Created by Irving on 2014/12/12.
+ */
+public class ServerPasswordCallback implements CallbackHandler {
+
+    private static final Map<String, String> userMap = new HashMap<String, String>();
+    static {
+        userMap.put("client", "Irving");
+        userMap.put("server", "123456");
+    }
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        WSPasswordCallback callback = (WSPasswordCallback) callbacks[0];
+
+        String clientUsername = callback.getIdentifier();
+        String serverPassword = userMap.get(clientUsername);
+
+        if (serverPassword != null) {
+            callback.setPassword(serverPassword);
+        }
+    }
+}
